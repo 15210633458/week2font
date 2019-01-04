7ef3fc113add52c3b2c75bc87be0d9512290f05e
sunyufeng <2220438231@qq.com>
2019-01-04 11:26:11 +0800

    页面完成

--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,29 @@
 require.config({
     paths: {
-        'scroll': './bscroll'
+        'scroll': './bscroll',
+        'jquery': './jquery'
     }
 })
-require(['scroll'], function(scroll) {
+require(['scroll', 'jquery'], function(scroll, jquery) {
     var con = document.getElementById('con');
+    var page = 1,
+        page_size = 5,
+        type = 1;
 
-
-    var xhr = new XMLHttpRequest();
-    xhr.open('get', '/api/get/list?page=1&page_size=5&type=1', true)
-    xhr.onload = function(res) {
-        if (res.target.status == 200) {
-            //console.log(res.target.response)
-            var data = JSON.parse(res.target.response)
+    jquery.ajax({
+        url: '/api/get/list',
+        data: {
+            page: page,
+            page_size: page_size,
+            type: type
+        },
+        success: function(data) {
             if (data.code == 1) {
                 var html = '';
                 data.data.forEach((i) => {
                     html += `<dl>
                                 <dt>
-                                        <img src="${i.img}" >
+                                        <img src="http://localhost:3000/images/${i.img}" >
                                     </dt>
                                 <dd>
                                     ${i.title}
@@ -28,6 +33,5 @@ require(['scroll'], function(scroll) {
                 con.innerHTML = html
             }
         }
-    }
-    xhr.send()
+    })
 })
